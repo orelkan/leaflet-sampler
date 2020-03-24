92e7508587a54ffd86a3cd924a58c6e4e4dceb8b
Orel Kanditan <orelkanditan@gmail.com>
2020-03-25 00:05:07 +0200

    Updates

---
+++ precache-manifest.8ebb8382e3222d8486a082c6b3b9beb9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3eff2d6121ce90aeb639b308b459b880",
+    "url": "/leaflet-sampler/index.html"
+  },
+  {
+    "revision": "9e5edb0f8ceaaa73836a",
+    "url": "/leaflet-sampler/static/css/main.24803a97.chunk.css"
+  },
+  {
+    "revision": "1fc23fccf619d894759b",
+    "url": "/leaflet-sampler/static/js/2.62906254.chunk.js"
+  },
+  {
+    "revision": "278be41389e3536a5347a9018737729d",
+    "url": "/leaflet-sampler/static/js/2.62906254.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9e5edb0f8ceaaa73836a",
+    "url": "/leaflet-sampler/static/js/main.ca82d59c.chunk.js"
+  },
+  {
+    "revision": "11c5e8e5f157a4980adb",
+    "url": "/leaflet-sampler/static/js/runtime-main.2d52ebf1.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/leaflet-sampler/precache-manifest.8ebb8382e3222d8486a082c6b3b9beb9.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/leaflet-sampler/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
